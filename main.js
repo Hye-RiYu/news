ddf51938940e2b752e0421fea3989f981bc45749
Hyeri Yu <yhr8340@naver.com>
2025-02-28 22:50:28 +0900

    fourth commit

--- main.js
+++ main.js
@@ -2,6 +2,30 @@ let newsList = [];
 const menus = document.querySelectorAll(".menus-button button")
 menus.forEach(menu => menu.addEventListener("click",(event) => getNewsByCategory(event)));
 
+let url = new URL(
+  `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr`
+ );
+
+const getNews = async () => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    if (response.status === 200) {
+      if (data.articles.length === 0) {
+        throw new Error("No result for this search");
+      }
+      newsList = data.articles;
+      render();
+    }else {
+      throw new Error(data.message);
+    }
+
+  } catch (error) {
+    errorRender(error.message);
+  }
+
+}
+
 const truncateSummary = (summary) => {
   if (!summary) {
     return "내용없음";
@@ -14,37 +38,21 @@ const truncateSummary = (summary) => {
 };
 
 const getLatestNews = async () => {
-  const url = new URL(
-    `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr`
-   );
-  const response = await fetch(url);
-  const data = await response.json()
-  newsList = data.articles;
-  render();
-  console.log('data', data);
-  console.log('newsList', newsList);
+  url = new URL (`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr`);
+  await getNews();
+
 };
 
 const getNewsByCategory = async (event) => {
   const category = event.target.textContent.toLowerCase();
-  console.log("category",category);
-  const url = new URL (`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&category=${category}`);
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log("ddd",data);
-  newsList = data.articles;
-  render();
+  url = new URL (`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&category=${category}`);
+  await getNews();
 };
 
 const getNewsByKeyword = async () => {
   const keyword = document.getElementById("search-input").value;
-  console.log("keyword", keyword)
-  const url = new URL (`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&q=${keyword}`);
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log("keyword data", data)
-  newsList = data.articles;
-  render();
+  url = new URL (`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&q=${keyword}`);
+  await getNews();
 };
 
 const render = () => {
@@ -74,6 +82,15 @@ const render = () => {
 
         console.log("html", newsHTML);
         document.getElementById('news-board').innerHTML=newsHTML;
+}
+
+const errorRender = (errorMessage) => {
+  const errorHTML = 
+    `<div class="alert alert-danger" role="alert">
+      ${errorMessage}
+    </div>`;
+
+    document.getElementById("news-board").innerHTML = errorHTML;
 }
 
 
